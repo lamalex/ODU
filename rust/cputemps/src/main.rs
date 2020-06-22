b5003a6d728a9ef1abcbfcf523b22dd0c910958f
Alexander Launi <alex.launi@gmail.com>
2020-06-21 21:56:01 -0400

    Parse args with clap instead of by hand

--- rust/cputemps/src/main.rs
+++ rust/cputemps/src/main.rs
@@ -1,30 +1,30 @@
+use clap::{crate_version, App, Arg};
 use cputemps::processor::{Processor, ProcessorError};
 use rayon::prelude::*;
 use std::env;
 
-fn help() {
-    let exe = match env::current_exe() {
-        Ok(exe) => exe.to_str().unwrap().to_owned(),
-        Err(_e) => String::from("(unidentified-executable)"),
-    };
-
-    println!("Hello. Thank you for trying to interpolate your cpu temperature data.");
-    println!("I regret to inform you that you must pass at least 1 file path to this program.");
-    println!("Try something like this:");
-    println!("\t{} <temperatures_1.txt> ...", exe);
-    println!("Output data can be found alongside input data with -out-core-#.txt appended.");
-    println!("🖖🏽 Live long and interpolate");
-}
-
 fn main() -> Result<(), ProcessorError> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        help();
-        std::process::exit(0x69)
-    }
+    const ABOUT: &'static str =
+        "Analyzes n-core CPU temperature data via interpolation, and least squares approximation.
+Output data can be found alongside input data with -out-core-#.txt appended.
+🖖🏽 Live long and interpolate.";
 
-    args[1..]
+    let matches = App::new("CS517 Semester Project: CPU Temperatur intep-o-matic")
+        .version(crate_version!())
+        .author("Alex L. Launi <alaun001@odu.edu>")
+        .about(ABOUT)
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Sets the input file(s) to use")
+                .required(true)
+                .multiple(true),
+        )
+        .get_matches();
+
+    let files: Vec<&str> = matches.values_of("INPUT").unwrap().collect();
+
+    files
         .par_iter()
-        .map(|data_file_path| Processor::process_data_file(&data_file_path[..]))
+        .map(|data_file_path| Processor::process_data_file(data_file_path))
         .collect()
 }
