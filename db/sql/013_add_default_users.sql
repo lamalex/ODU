966716da569e8f5d9a7dc3a136ad6a5bed835e86
Alex Launi <alaun001@odu.edu>
2021-04-17 18:50:43 -0400

    Set default passwords to something memorable instead of random nonsense (#31)

--- db/sql/013_add_default_users.sql
+++ db/sql/013_add_default_users.sql
@@ -1,6 +1,6 @@
 INSERT INTO tbl_fact_users (name, email, password, created_date, updated_date, user_role, department) VALUES
-        ('Alex Launi', 'alex@example.com', '$2y$10$MTfXNXdlax9zpiyWAh0.7uUSKZX81VfWOAFks7PhFAiahndV9Jpq.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ADMINISTRATOR', 1),
-        ('Josh Farr', 'josh@example.com', '$2y$10$tmJbAFwDx/meIsWpDxVcp.j/hNXtDZQBtnYv5ZuPYm1pGr3EwftNG', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ADMINISTRATOR', 1),
-        ('Jason Rich', 'jason@example.com', '$2y$10$lASdMGCqqFAw5giMAy1AN.QcK5MlJbQ./R2YZfKBnUFTcqk.mHkse', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ADMINISTRATOR', 1),
-        ('Suraksha Tasgaonkar', 'suraksha@example.com', '$2y$10$gEkssYEC4UfW6WB0W9UD1uM8b7lunVnxS8IW2dAGhCOeYMMjov0wy', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ADMINISTRATOR', 1),
-        ('Hot Dog', 'nothotdog@example.com', '$2y$10$g4jtfSNDTRoSN6dExddjP.vtg1LWoNJPgSr2JuRR9Hpgkf1HQ5w3q', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ADMINISTRATOR', 1);
+        ('Alex Launi', 'alex@example.com', '$2y$10$WHtsJKjF4jOyGrwP4b7e6u..A2Gz3ufHLTmT9MJQlEEUZWUZ2PDUy', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ADMINISTRATOR', 1),
+        ('Josh Farr', 'josh@example.com', '$2y$10$2DQPJFIzVddGzCmxNYBIcO6PKfzXDR4753ctJOrz3HYsE55wJEfsm', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ADMINISTRATOR', 1),
+        ('Jason Rich', 'jason@example.com', '$2y$10$v2K.hCNJua2TfDhm8Um0murymsmL9QR7Sb6PZEP9H/JFRBqBs3KaO', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ADMINISTRATOR', 1),
+        ('Suraksha Tasgaonkar', 'suraksha@example.com', '$2y$10$MwQsIPMICT5PIUN0v1Mr.udkLCuZuijxjxZ5T7B7sndj2VJ/suwOu', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ADMINISTRATOR', 1),
+        ('Hot Dog', 'nothotdog@example.com', '$2y$10$rE8bATLMibhHfEkSNL8usuhP2sjvSC16if0.RVIfkOmWb04rNopEi', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ADMINISTRATOR', 1);
